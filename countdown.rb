f8149b6918067c215e2f208e1fc7d578a07d8574
Moringa School <maryanneanyango98@gmail.com>
2022-08-21 14:35:28 +0300

    commit tree 76c0514bf3d7b8d1f807b1ca8b8fce84ae49a75b

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
+midnight = 5
 
-def countdown
+def countdown(number)
+    while number > 0
+      puts "#{number} SECOND(S)!"
+      number -= 1
+    end
+    # puts "HAPPY NEW YEAR!"
+    "HAPPY NEW YEAR!"
+  end
+
+
+def countdown_with_sleep (midnight)
+    while midnight > 0
+        puts "#{midnight}  SECOND(S)! "
+        sleep (1)
+        midnight -=1
+    end
+    "HAPPY NEW YAER!"
 end
+
+puts countdown (midnight)
+puts ""
+puts countdown_with_sleep (midnight)
